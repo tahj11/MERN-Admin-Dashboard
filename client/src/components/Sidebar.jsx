52d1cdebd2da07e138f549570c3fbbfb93e1987b
tahj11 <jtahj25@gmail.com>
2023-04-12 12:05:57 -0500

    Navbar and Sidebar updated to have user logged in details

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   Box,
   Divider,
@@ -28,9 +28,10 @@ import {
   TrendingUpOutlined,
   PieChartOutlined,
 } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import FlexBetween from "./FlexBetween";
 import profileImage from "assets/profile_pic.jpg";
-import { useLocation, useNavigate } from "react-router-dom";
 
 const navItems = [
   {
@@ -70,7 +71,7 @@ const navItems = [
     icon: <TodayOutlined />,
   },
   {
-    text: "Month",
+    text: "Monthly",
     icon: <CalendarMonthOutlined />,
   },
   {
@@ -92,6 +93,7 @@ const navItems = [
 ];
 
 const Sidebar = ({
+  user,
   drawerWidth,
   isSidebarOpen,
   setIsSidebarOpen,
@@ -105,6 +107,7 @@ const Sidebar = ({
   useEffect(() => {
     setActive(pathname.substring(1));
   }, [pathname]);
+
   return (
     <Box component="nav">
       {isSidebarOpen && (
@@ -118,17 +121,19 @@ const Sidebar = ({
             "& .MuiDrawer-paper": {
               color: theme.palette.secondary[200],
               backgroundColor: theme.palette.background.alt,
-              boxSizing: "border-box",
+              boxSixing: "border-box",
               borderWidth: isNonMobile ? 0 : "2px",
               width: drawerWidth,
             },
           }}
         >
-          <Box width="100%">
-            <Box m="1.5rem 2rem 2rem 3rem">
+          <Box width="100%" mb="8rem">
+            <Box m="1.5rem 2rem 3rem 3rem">
               <FlexBetween color={theme.palette.secondary.main}>
                 <Box display="flex" alignItems="center" gap="0.5rem">
-                  <Typography>ECOMVISION </Typography>
+                  <Typography variant="h4" fontWeight="bold">
+                    ECOMVISION
+                  </Typography>
                 </Box>
                 {!isNonMobile && (
                   <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
@@ -186,6 +191,42 @@ const Sidebar = ({
                 );
               })}
             </List>
+          </Box>
+
+          <Box position="relative" bottom="2rem">
+            <Divider />
+            <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="40px"
+                width="40px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.9rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.8rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <SettingsOutlined
+                sx={{
+                  color: theme.palette.secondary[300],
+                  fontSize: "25px ",
+                }}
+              />
+            </FlexBetween>
           </Box>
         </Drawer>
       )}
--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+//data imports
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 /* CONFIGURATION */
 dotenv.config();
 const app = express();
@@ -36,5 +40,8 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`));
+
+    /* ONLY ADD DATA ONE TIME */
+    // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));

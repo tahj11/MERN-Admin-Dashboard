a6a27c52b0f35cd024051ef8e068300efc7c8632
tahj11 <jtahj25@gmail.com>
2023-04-12 14:19:51 -0500

    Product Page completed

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,13 +3,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3400" }),
   reducerPath: "adminApi",
-  tagTypes: ["User"],
+  tagTypes: ["User", "Product"],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
       providesTags: ["User"],
     }),
+    getProducts: build.query({
+      query: () => "client/products",
+      providesTags: ["Products"],
+    }),
   }),
 });
 
-export const { useGetUserQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery } = api;
--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ import salesRoutes from "./routes/sales.js";
 
 //data imports
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -42,6 +44,8 @@ mongoose
     app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`));
 
     /* ONLY ADD DATA ONE TIME */
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
     // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
